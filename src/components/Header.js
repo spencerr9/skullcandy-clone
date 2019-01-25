ffd56f1df99210b2f0a1482f2b750f98f1552450
Spencer Ricks <spencerricks@Spencers-MBP.ricks.inet>
2019-01-24 23:19:10 -0700

    styled left half of nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import skullWht from '../images/skullLogowht.png';
 
 export default function Header(){
     return (
         <div>
-            <h1>Header Component</h1>
+            <header>
+                <div className='displayFlex' style={{padding: '0 4vw'}}>
+                    <img src={skullWht} alt="" style={{height: '32px'}} ></img>
+                    <div style={{fontSize: '32px', fontWeight: 'bold', letterSpacing: '-1px'}}>Skullcandy</div>
+                    <div className='displayFlex' style={{padding: '0 0 0 2vw', fontSize: '12px'}}>
+                        <div>SHOP</div>
+                        <div style={{padding: '0 0 0 2vw'}} >DISCOVER</div>
+                    </div>
+                </div>
+                
+            </header>
         </div>
     )
 }
