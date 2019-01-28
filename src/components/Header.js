d0be94a5877f431f028af183c1dd2c69418bb0cd
Spencer Ricks <spencerricks@Spencers-MBP.ricks.inet>
2019-01-27 21:30:31 -0700

    Transferred in-line styling to from header component to css sheet

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,26 +6,25 @@ import user from '../images/outline-user-24px.svg';
 
 export default function Header(){
     return (
-        <div>
-            <header style={{padding: '0 5vw 0 4vw', justifyContent: 'space-between'}} >
-                <div className='displayFlex' >
-                    <img src={skullWht} alt="" style={{height: '32px'}} ></img>
-                    <div style={{fontSize: '32px', fontWeight: 'bold', letterSpacing: '-1px'}}>Skullcandy</div>
-                    <div className='displayFlex' style={{padding: '0 0 0 2vw', fontSize: '12px'}}>
-                        <a href="www.google.com" style={{padding: '10px 0'}}>SHOP</a>
-                        <a href="www.google.com" style={{margin: '0 0 0 2vw', padding: '10px 0'}} >DISCOVER</a>
+        <div className='headerContainer'>
+            <header>
+                <div className='leftSide displayFlex' >
+                    <img className='skullWht' src={skullWht} alt=""></img>
+                    <div className='skullcandyText'>Skullcandy</div>
+                    <div className='leftTextContainer displayFlex'>
+                        <a href="www.google.com">SHOP</a>
+                        <a className='discover' href="www.google.com">DISCOVER</a>
                     </div>
                 </div>
-                <div className='displayFlex'  >
-                    <div className='displayFlex' style={{padding: '0 0 0 2vw', fontSize: '11px'}}>
-                        <a href="www.google.com" style={{margin: '0 1.5vw 0 0', padding: '10px 0', color: '#999'}} >Location</a>
-                        <a href="www.google.com" style={{margin: '0 1.5vw 0 0', padding: '10px 0', color: '#999'}} >Support</a>
+                <div className='rightSide displayFlex'>
+                    <div className='rightTextContainer displayFlex'>
+                        <a href="www.google.com">Location</a>
+                        <a href="www.google.com">Support</a>
                     </div>
-                    <img src={user} alt="" style={{padding: '0 1.5vw 0 0', height: '33px'}} ></img>
-                    <img src={search} alt="" style={{padding: '0 2vw 0 0', height: '29px'}} ></img>
-                    <img src={shoppingBag} alt="" style={{}} ></img>
+                    <img className='userImg' src={user} alt=""></img>
+                    <img className='searchImg' src={search} alt=""></img>
+                    <img src={shoppingBag} alt=""></img>
                 </div>
-                
             </header>
         </div>
     )
