db79d32ec464cfb883bd604f776fed7a7325e387
Spencer Ricks <spencerricks@Spencers-MBP.ricks.inet>
2019-02-12 23:55:26 -0700

    Added a title to the landing screen page

--- src/components/LandingScreen/LandingScreen.js
+++ src/components/LandingScreen/LandingScreen.js
@@ -4,13 +4,16 @@ import './LandingScreen.css';
 
 export default function LandingScreen(){
     return (
-        <div style={{display: 'flex'}}>
-            <div style={{backgroundColor: 'pink', height: '94vh', width: '64vw'}}>
-                <div></div>
-            </div>
-            <div>
-                <div style={{backgroundColor: 'red', height: '47vh', width: '33vw'}}></div>
-                <div style={{backgroundColor: 'blue', height: '47vh', width: '33vw'}}></div>
+        <div style={{display: 'flex', height: '94vh', width: '100vw'}}>
+            <div className='landing' style={{height: '100%', width: '72%'}}></div>
+            <div style={{height: '100%', width: '36vw', backgroundColor: 'green'}}>
+                <div className='topLanding' style={{backgroundColor: 'red', height: '50%', width: '100%'}}>
+                    <div style={{padding: '25px 25px'}}>STACK YOUR CART TO <br></br>
+                        SEE WHAT YOU <br></br>
+                        QUALIFY FOR
+                    </div>
+                </div>
+                <div className='bottomLanding' style={{backgroundColor: 'blue', height: '50%', width: '100%'}}></div>
             </div>
         </div>
     )
